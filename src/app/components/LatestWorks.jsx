c6a8ee9cccc2dc31c9a9b970e14088371333a6cd
James <jkhallpurpose@gmail.com>
2023-12-08 02:24:32 -0500

    9th

--- src/app/components/LatestWorks.jsx
+++ src/app/components/LatestWorks.jsx
@@ -99,14 +99,9 @@ const LatestWorks = () => {
     cssEase: 'linear',
   };
   return (
-    <main>
-      <h2 className=" text-center text-4xl font-bold  text-white dark:text-light mt-4 mb-8 md:mb-12 ">
-        <span
-          className="border-2 rounded-3xl border-white dark:border-light p-2 opacity-75
-        bg-light dark:bg-dark"
-        >
-          Portfolio Projects
-        </span>
+    <main className="py-12">
+      <h2 className=" text-center text-4xl font-bold  text-gray dark:text-dark mt-4 mb-8 md:mb-12 opacity-75">
+        <span>Portfolio Projects</span>
       </h2>
       <div>
         {/* CSS files for react-slick */}
