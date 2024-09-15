671a8e2e6eb506404fa96040faa315e27305ab23
James <jkhallpurpose@gmail.com>
2024-09-14 21:30:44 -0400

    sep_14

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import { cx } from './components/utils';
 import siteMetadata from './components/utils/siteMetaData';
 import { ThemeProvider } from '@mui/material';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -41,6 +42,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
