7b5381ef76e52f3b2f0bdb0d90e40279f9818683
James <jkhallpurpose@gmail.com>
2024-10-03 11:24:39 -0400

    oct_3

--- src/app/components/utils/siteMetaData.js
+++ src/app/components/utils/siteMetaData.js
@@ -2,7 +2,8 @@ const siteMetadata = {
   title: 'James K Howard - Portfolio Site',
   author: 'James K Howard',
   headerTitle: 'James K Howard - Portfolio',
-  description: 'James K Howard: Portfolio Site',
+  description:
+    'James K Howard: Portfolio Site. Browse to learn more about me, see my latest activity, reference my blog posts, peruse my experience and more.',
   content: 'Portfolio site, showcasing my latest works accompanied with a bio.',
   language: 'en-us',
   theme: 'system',
